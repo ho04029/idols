e245a1b25cdbd3ead66fb8b44e2f2971135a8831
seokgyeong lee <ho04029@naver.com>
2024-11-25 17:25:35 +0900

    feat: 404페이지
    1. 404페이지
    2. 프로젝트 타이틀 수정

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="flex justify-center">
+      <div className="font-['Loboto'] text-[#151147] absolute right-1/2 top-1/2 translate-x-1/2 -translate-y-1/2 w-[430px] max-w-full overflow-hidden">
+        <img
+          src="https://i.ibb.co/TLgjMDs/404error.png"
+          alt="error"
+          className="mb-[20px]"
+        />
+        <p className="text-center mb-[13px] text-[16px] font-semibold">
+          죄송합니다. <br />
+          요청하신 페이지를 찾을 수 없습니다.
+        </p>
+        <p className="text-center mb-[117px] text-[14px] font-medium break-keep">
+          찾으시는 페이지의 주소가 잘못 입력되었거나, 페이지의 주소가 변경,
+          삭제되어 요청하는 페이지를 찾을 수 없습니다. 아래 다른 서비스를 이용
+          해주십시오.
+        </p>
+        <div className="flex justify-between">
+          <button className="w-[160px] bg-[#151147] text-[#FFFFFF] text-[16px] font-semibold py-[7px] rounded-[16px]">
+            이전 페이지로 이동
+          </button>
+          <button className="w-[160px] bg-[#151147] text-[#FFFFFF] text-[16px] font-semibold py-[7px] rounded-[16px]">
+            홈으로 이동
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
