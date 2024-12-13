a50784f111c8865aee854193deaf781e548b6cf6
seokgyeong lee <ho04029@naver.com>
2024-12-13 17:35:24 +0900

    feat: 검색창

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,5 +1,6 @@
 import { useMenuStore } from "../store/store";
 import { AiOutlineClose } from "react-icons/ai";
+import SearchBox from "./SearchBox";
 
 const MenuBar = () => {
   const closeMenuHandler = useMenuStore((state) => state.closeMenuHandler);
@@ -9,6 +10,9 @@ const MenuBar = () => {
         onClick={closeMenuHandler}
         className="text-[25px] lg:text-[35px] mt-[41px] lg:mt-[50px] mb-[29px] lg:mb-[45px] ml-[23px] lg:ml-[46px] text-black cursor-pointer"
       />
+      <div className="flex flex-col items-center">
+        <SearchBox className="w-[253px] lg:w-[445px] h-[50px] lg:h-[88px] text-[16px] lg:text-[25px]" />
+      </div>
     </div>
   );
 };
---
+++ src/components/SearchBox.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { IoIosSearch } from "react-icons/io";
+
+const SearchBox = ({ className }: { className?: string }) => {
+  return (
+    <div
+      className={`relative flex items-center py-[5px] rounded-[20px] border-2 border-solid border-[#6D6D6D] shadow-md ${className}`}
+    >
+      <button className="mx-[15px] text-[#6D6D6D]">
+        <IoIosSearch />
+      </button>
+      <input
+        type="text"
+        placeholder="Search"
+        className="w-full h-full pl-[10px] py-[17px] bg-transparent  border-l-2"
+      />
+    </div>
+  );
+};
+
+export default SearchBox;
