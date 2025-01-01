88b32868c7b6b5da1ba03b5f2acc90f12709c127
seokgyeong lee <ho04029@naver.com>
2025-01-01 18:22:23 +0900

    design: 판매그래프페이지 디자인 수정

--- src/pages/SalesGraph.tsx
+++ src/pages/SalesGraph.tsx
@@ -8,6 +8,7 @@ import {
   PointElement,
   LineElement,
   Tooltip,
+  ChartOptions,
 } from "chart.js";
 
 import { IdolGroups } from "../data/idolgroup";
@@ -33,43 +34,58 @@ const labels = ["1일차", "2일차", "3일차", "4일차", "5일차", "6일차"
 
 const SalesGraph = () => {
   const [selectedAlbum, setSelectedAlbum] = useState<IdataSet[]>([]);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const location = useLocation();
 
   const groupName = location.pathname.split("/")[2];
   const group = IdolGroups[groupName];
   const albumDataSet = albumSales[groupName];
 
-  const options = {
+  const options: ChartOptions<"line"> = {
     interaction: {
-      intersect: false,
+      intersect: true,
     },
+    maintainAspectRatio: false, //부모사이즈에 맞춰 반응
     scales: {
       x: {
         grid: {
           display: true,
-          color: [
-            group.textColor,
-            "transparent",
-            "transparent",
-            "transparent",
-            "transparent",
-            "transparent",
-            "transparent",
-          ],
+          color: (ctx) =>
+            ctx.tick.value === 0 ? group.textColor : "transparent",
           drawTicks: false,
+          lineWidth: (ctx) => {
+            if (windowWidth < 1024) {
+              return ctx.tick.value === 0 ? 2 : 1;
+            }
+            return ctx.tick.value === 0 ? 4 : 2;
+          },
         },
         ticks: {
           color: group.textColor, //폰트컬러
+          padding: windowWidth < 1024 ? 15 : 25, //축과 tick사이 거리
+          font: {
+            size: windowWidth < 1024 ? 14 : 25,
+          },
         },
       },
       y: {
         grid: {
           color: group.textColor,
           drawTicks: false, //축 뒤로 선 빠져나오는 거
+          lineWidth: (ctx) => {
+            if (windowWidth < 1024) {
+              return ctx.tick.value === 0 ? 2 : 1;
+            }
+            return ctx.tick.value === 0 ? 4 : 2;
+          },
         },
         ticks: {
           maxTicksLimit: 4, // Y축 라벨을 최대 6개로 제한
           color: group.textColor, //폰트컬러
+          padding: windowWidth < 1024 ? 8 : 25,
+          font: {
+            size: windowWidth < 1024 ? 14 : 25,
+          },
         },
       },
     },
@@ -122,6 +138,7 @@ const SalesGraph = () => {
       data: albumData.albumSalesData,
       backgroundColor: albumData.mainColor,
       borderColor: albumData.mainColor,
+      borderWidth: windowWidth < 1024 ? 1 : 2,
     },
   ];
 
@@ -134,15 +151,21 @@ const SalesGraph = () => {
     setSelectedAlbum([]);
   }, [groupName]);
 
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <div
       className="w-full min-h-screen flex flex-col items-center"
       style={{ backgroundColor: group.bgColor, color: group.textColor }}
     >
       <Header />
-      <main className="w-full">
-        <section>
-          <ul className="flex flex-wrap gap-[15px]">
+      <main className="w-full flex flex-col gap-[82px] lg:gap-[130px]">
+        <section className="w-full flex flex-col gap-[28px] lg:gap-[68px]">
+          <ul className="flex flex-wrap gap-[15px] lg:gap-[50px] pl-[58px] lg:pl-[232px]">
             {selectedAlbum.map((data, idx) => (
               <li
                 key={idx}
@@ -152,16 +175,20 @@ const SalesGraph = () => {
               >
                 <div
                   style={{ backgroundColor: data.borderColor }}
-                  className="w-[10px] h-[10px] rounded-full"
+                  className="w-[10px] h-[10px] lg:w-[20px] lg:h-[20px] rounded-full"
                 ></div>
-                <p className="font-bold text-[14px]">{data.label}</p>
+                <p className="font-bold text-[14px] lg:text-[20px]">
+                  {data.label}
+                </p>
               </li>
             ))}
           </ul>
-          <Line options={options} data={data} />
+          <div className="w-full h-[312px] lg:h-[543px] px-[12px] lg:px-[113px]">
+            <Line options={options} data={data} />
+          </div>
         </section>
-        <section>
-          <ul className="grid grid-cols-3 lg:grid-cols-4 gap-x-[42px] lg:gap-x-[135px] gap-y-[30px] lg:gap-y-[43px]">
+        <section className="w-full flex flex-col items-center">
+          <ul className="max-w-[354px] md:max-w-[861px] grid grid-cols-3 md:grid-cols-4 gap-x-[42px] lg:gap-x-[135px] gap-y-[30px] lg:gap-y-[43px]">
             {Object.keys(albumDataSet).map((title, idx) => (
               <li
                 key={idx}
@@ -169,7 +196,7 @@ const SalesGraph = () => {
                 className="flex flex-col items-center gap-[15px] cursor-pointer"
               >
                 <img src={albumDataSet[title].img} alt={title} />
-                <p className="text-center font-bold text-[16px] lg:text-[20px] leading-[10px]">
+                <p className="text-center font-bold text-[16px] lg:text-[20px]">
                   {title}
                 </p>
               </li>
