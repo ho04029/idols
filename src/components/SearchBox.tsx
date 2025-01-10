ad484d5801b6444f93bceca6545dd0bac640e9e7
seokgyeong lee <ho04029@naver.com>
2025-01-10 17:35:57 +0900

    feat: 검색 기능

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,20 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { IoIosSearch } from "react-icons/io";
 
+const theboyzKeyword = ["더보이즈", "theboyz", "the boyz"];
+const zb1Keyword = ["제로베이스원", "제베원", "zb1", "zerobaseone"];
+const riizeKeyword = ["라이즈", "riize"];
+const nctwishKeyword = [
+  "엔시티 위시",
+  "엔시테위시",
+  "엔위시",
+  "위시",
+  "nct wish",
+  "nctwish",
+];
+
+const keywordMap: { [key: string]: string[] } = {
+  "/idolgroup/THEBOYZ": theboyzKeyword,
+  "/idolgroup/ZEROBASEONE": zb1Keyword,
+  "/idolgroup/RIIZE": riizeKeyword,
+  "/idolgroup/NCTWISH": nctwishKeyword,
+};
+
 const SearchBox = ({ className }: { className?: string }) => {
+  const [searchKeyword, setSearchKeyword] = useState<string>("");
+  const navigate = useNavigate();
+
+  const submitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+    const lowerSearchKeyword = searchKeyword.toLowerCase();
+
+    const matchingRoute = Object.keys(keywordMap).find((route) =>
+      keywordMap[route].includes(lowerSearchKeyword)
+    );
+
+    if (matchingRoute) {
+      navigate(matchingRoute);
+    } else {
+      console.log("No matching group found");
+    }
+  };
+
   return (
-    <div
+    <form
       className={`relative flex items-center py-[5px] rounded-[20px] border-2 border-solid border-[#6D6D6D] shadow-md ${className}`}
+      onSubmit={submitHandler}
     >
-      <button className="mx-[15px] text-[#6D6D6D]">
+      <button className="mx-[15px] text-[#6D6D6D]" type="submit">
         <IoIosSearch />
       </button>
       <input
         type="text"
         placeholder="Search"
         className="w-full h-full pl-[10px] py-[17px] bg-transparent  border-l-2 focus:outline-none"
+        onChange={(e) => setSearchKeyword(e.target.value)}
       />
-    </div>
+    </form>
   );
 };
 
---
+++ src/pages/NoSearchResult.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NoSearchResult = () => {
+  return <div></div>;
+};
+
+export default NoSearchResult;
